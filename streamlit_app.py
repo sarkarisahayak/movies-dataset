0a4fdb52e012b08a12f62e296e3647cc1dc984bf
sarkarisahayak <sarkarisahayak13@gmail.com>
2024-08-19 15:24:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write(
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/can.csv")
+    df = pd.read_csv("data/movies_genres_summary.csv")
     return df
 
 
